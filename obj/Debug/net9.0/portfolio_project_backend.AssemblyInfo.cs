903b979f50d50bf2c5c6d2d448b387c31fc8f21a
alexander baker <alexbaker5795@gmail.com>
2025-05-16 22:21:12 -0500

    Update

--- obj/Debug/net9.0/portfolio_project_backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/portfolio_project_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio_project_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd8c0efdedef661c0aeca8374b5ae46b28355469")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio_project_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio_project_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
